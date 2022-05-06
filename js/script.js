29eac99d9e01fb8c9794d07b7d3ac7d8987db53d
PattyCutie <iamsureiamcute@gmail.com>
2022-05-06 17:36:29 +0200

    added Typing Animation

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/* ######## Typing Animation ######## */
+var typed = new Typed(".typing", {
+    strings: [" ", "Web Developer", "Web Designer", "Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed:60
+})
